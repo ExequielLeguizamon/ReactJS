1234b72a2d8012f8d44b13ed021b86aa32605cbc
Exequiel Leguizamon <jorgeexequielleguizamon@gmail.com>
2022-06-29 12:22:16 -0300

    Desafío: Catálogo con MAPS y Promises

--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemCount from '../components/ItemCount/ItemCount'
+import ItemList from '../components/ItemList/ItemList'
 
 const ItemListContainer = ({greeting}) => {
+
+  const [productos, setProductos] = useState(null)
+
+  useEffect(() => {
+
+    const getProductos = async () => {
+      try {
+        const response = await fetch('/mock/data.json');
+        const data = await response.json();
+        console.log(data);
+        setProductos(data);
+      } catch (error) {
+        console.log("Hubo un error:");
+        console.log(error);
+      }
+    }
+
+  getProductos()
+
+  }, [])
+
+console.log(productos);
+
 
 const handleAdd = () => {
   console.log("Se agrego al carrito")
@@ -11,6 +35,11 @@ const handleAdd = () => {
     <div>
       <h1>{greeting}</h1>
       <ItemCount handleAdd={handleAdd}/>
+      {productos ?
+        <ItemList products={productos} />
+        :
+        null
+      }
     </div> 
   )
 }
